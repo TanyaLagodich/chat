54f4dbe7aecf14683d7f6611def65aa673ef7e71
Tatiana Lagodich <t.lagodich@amarkets.com>
2023-10-08 17:31:53 +0400

    server: create structure

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// app.use('/api/users', require('./routes/users'));
+// app.use('/api/messages', require('./routes/messages'));
+
+// Подключение посредников и другой конфигурации
+
+module.exports = app;
